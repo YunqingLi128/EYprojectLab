bca31689582e0d8b6b8955f9f599c7cd355bedcb
Wenzhou Yuan <wenzhouy@uchicago.edu>
2021-03-05 02:34:59 -0600

    Update LineChart.vue

--- demo/frontend/projectlabdemp/src/components/LineChart.vue
+++ demo/frontend/projectlabdemp/src/components/LineChart.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-    <button @click="getData();">GET DATA</button>
-    <div id="market-risk-weighted-assets" :style="{width: '100%', height: '600px'}"></div>
+  <div class="myDiv">
+    <button @click="getData();getData_2();getData_3();">GET DATA</button>
+    <div id="change-in-var-measure-overtime" :style="{width: '50%', height: '600px'}"></div>
+    <div id="market-risk-weighted-assets" :style="{width: '50%', height: '600px'}"></div>
+    <div id="svar-var-ratio-overtime" :style="{width: '50%', height: '600px'}"></div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import * as echarts from 'echarts';
-
 export default {
   name: 'LineChart',
   data: function () {
@@ -18,6 +19,32 @@ export default {
   },
   methods: {
     drawLineChart () {
+      let that = this;
+      let chartDom = document.getElementById('change-in-var-measure-overtime');
+      let myChart = echarts.init(chartDom)
+      let option = {
+        title: {
+          text: 'Change in VaR-based Measure Over Time'
+        },
+        legend: {
+          bottom: 0,
+          data: that.lineChartData.legend
+        },
+        xAxis: {
+          type: 'category',
+          data: that.lineChartData.xAxisData
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        series: that.lineChartData.series
+      }
+      myChart.setOption(option);
+    },
+    drawLineChart2 () {
       let that = this;
       let chartDom = document.getElementById('market-risk-weighted-assets');
       let myChart = echarts.init(chartDom)
@@ -43,7 +70,72 @@ export default {
       }
       myChart.setOption(option);
     },
+    drawLineChart3 () {
+      let that = this;
+      let chartDom = document.getElementById('svar-var-ratio-overtime');
+      let myChart = echarts.init(chartDom)
+      let option = {
+        title: {
+          text: 'sVaR to VaR Ratio Over Time'
+        },
+        legend: {
+          bottom: 0,
+          data: that.lineChartData.legend
+        },
+        xAxis: {
+          type: 'category',
+          data: that.lineChartData.xAxisData
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        series: that.lineChartData.series
+      }
+      myChart.setOption(option);
+    },
     getData () {
+      let that = this;
+      that.chartData = {};
+      // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
+      const start = '2015Q3';
+      const end = '2016Q3';
+      const base = 'http://127.0.0.1:5000/getChangeInVaRBasedMeasureOvertime';
+      axios
+        .get(base, {
+          params: {
+            'start': start,
+            'end': end
+          }
+        })
+        .then(function (response) {
+          let data = response.data
+          let companies = []
+          let series = []
+          for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+              companies.push(key)
+              let chartItem = {}
+              chartItem.name = key
+              chartItem.type = 'line'
+              chartItem.data = []
+              for (const item of data[key]) {
+                chartItem.data.push(item[1])
+              }
+              series.push(chartItem)
+            }
+          }
+          console.log(companies)
+          console.log(series)
+          that.chartData.legend = companies
+          that.lineChartData.xAxisData = ['2015Q3', '2015Q4', '2016Q1', '2016Q2', '2016Q3'] // TODO: write function
+          that.lineChartData.series = series
+          that.drawLineChart()
+        });
+    },
+    getData_2 () {
       let that = this;
       that.chartData = {};
       // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
@@ -79,7 +171,46 @@ export default {
           that.chartData.legend = companies
           that.lineChartData.xAxisData = ['2015Q3', '2015Q4', '2016Q1', '2016Q2', '2016Q3'] // TODO: write function
           that.lineChartData.series = series
-          that.drawLineChart()
+          that.drawLineChart2()
+        });
+    },
+    getData_3 () {
+      let that = this;
+      that.chartData = {};
+      // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
+      const start = '2015Q3';
+      const end = '2016Q3';
+      const base = 'http://127.0.0.1:5000/getVaRsVaRRatioOvertime';
+      axios
+        .get(base, {
+          params: {
+            'start': start,
+            'end': end
+          }
+        })
+        .then(function (response) {
+          let data = response.data
+          let companies = []
+          let series = []
+          for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+              companies.push(key)
+              let chartItem = {}
+              chartItem.name = key
+              chartItem.type = 'line'
+              chartItem.data = []
+              for (const item of data[key]) {
+                chartItem.data.push(item[1])
+              }
+              series.push(chartItem)
+            }
+          }
+          console.log(companies)
+          console.log(series)
+          that.chartData.legend = companies
+          that.lineChartData.xAxisData = ['2015Q3', '2015Q4', '2016Q1', '2016Q2', '2016Q3'] // TODO: write function
+          that.lineChartData.series = series
+          that.drawLineChart3()
         });
     }
   }
@@ -89,3 +220,4 @@ export default {
 <style scoped>
 
 </style>
+
