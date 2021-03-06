51ab9e16ba99b94d21e71ec9a2412c7c0ffbe64e
Wenzhou Yuan <wenzhouy@uchicago.edu>
2021-03-06 02:18:37 -0600

    Optimize Line Chart codes

--- demo/frontend/projectlabdemp/src/components/LineChart.vue
+++ demo/frontend/projectlabdemp/src/components/LineChart.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="myDiv">
-    <button @click="getData();getData_2();getData_3();">GET DATA</button>
-    <div id="change-in-var-measure-overtime" :style="{width: '50%', height: '600px'}"></div>
-    <div id="market-risk-weighted-assets" :style="{width: '50%', height: '600px'}"></div>
-    <div id="svar-var-ratio-overtime" :style="{width: '50%', height: '600px'}"></div>
+    <button @click="getData('Change In VaR Measure Overtime');getData('Market Risk-Weighted Assets Overtime');getData('sVaR-VaR Ratio Overtime');">GET DATA</button>
+    <div id="Change In VaR Measure Overtime" :style="{width: '50%', height: '600px'}"></div>
+    <div id="Market Risk-Weighted Assets Overtime" :style="{width: '50%', height: '600px'}"></div>
+    <div id="sVaR-VaR Ratio Overtime" :style="{width: '50%', height: '600px'}"></div>
   </div>
 </template>
 
@@ -18,13 +18,13 @@ export default {
     }
   },
   methods: {
-    drawLineChart () {
+    drawLineChart (id) {
       let that = this;
-      let chartDom = document.getElementById('change-in-var-measure-overtime');
+      let chartDom = document.getElementById(id);
       let myChart = echarts.init(chartDom)
       let option = {
         title: {
-          text: 'Change in VaR-based Measure Over Time'
+          text: id
         },
         legend: {
           bottom: 0,
@@ -44,59 +44,7 @@ export default {
       }
       myChart.setOption(option);
     },
-    drawLineChart2 () {
-      let that = this;
-      let chartDom = document.getElementById('market-risk-weighted-assets');
-      let myChart = echarts.init(chartDom)
-      let option = {
-        title: {
-          text: 'Advanced market risk-weighted assets over time'
-        },
-        legend: {
-          bottom: 0,
-          data: that.lineChartData.legend
-        },
-        xAxis: {
-          type: 'category',
-          data: that.lineChartData.xAxisData
-        },
-        yAxis: {
-          type: 'value'
-        },
-        tooltip: {
-          trigger: 'axis'
-        },
-        series: that.lineChartData.series
-      }
-      myChart.setOption(option);
-    },
-    drawLineChart3 () {
-      let that = this;
-      let chartDom = document.getElementById('svar-var-ratio-overtime');
-      let myChart = echarts.init(chartDom)
-      let option = {
-        title: {
-          text: 'sVaR to VaR Ratio Over Time'
-        },
-        legend: {
-          bottom: 0,
-          data: that.lineChartData.legend
-        },
-        xAxis: {
-          type: 'category',
-          data: that.lineChartData.xAxisData
-        },
-        yAxis: {
-          type: 'value'
-        },
-        tooltip: {
-          trigger: 'axis'
-        },
-        series: that.lineChartData.series
-      }
-      myChart.setOption(option);
-    },
-    getData () {
+    getData (id) {
       let that = this;
       that.chartData = {};
       // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
@@ -132,85 +80,7 @@ export default {
           that.chartData.legend = companies
           that.lineChartData.xAxisData = ['2015Q3', '2015Q4', '2016Q1', '2016Q2', '2016Q3'] // TODO: write function
           that.lineChartData.series = series
-          that.drawLineChart()
-        });
-    },
-    getData_2 () {
-      let that = this;
-      that.chartData = {};
-      // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
-      const start = '2015Q3';
-      const end = '2016Q3';
-      const base = 'http://127.0.0.1:5000/getAdvancedMarketRiskWeightedAssets';
-      axios
-        .get(base, {
-          params: {
-            'start': start,
-            'end': end
-          }
-        })
-        .then(function (response) {
-          let data = response.data
-          let companies = []
-          let series = []
-          for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-              companies.push(key)
-              let chartItem = {}
-              chartItem.name = key
-              chartItem.type = 'line'
-              chartItem.data = []
-              for (const item of data[key]) {
-                chartItem.data.push(item[1])
-              }
-              series.push(chartItem)
-            }
-          }
-          console.log(companies)
-          console.log(series)
-          that.chartData.legend = companies
-          that.lineChartData.xAxisData = ['2015Q3', '2015Q4', '2016Q1', '2016Q2', '2016Q3'] // TODO: write function
-          that.lineChartData.series = series
-          that.drawLineChart2()
-        });
-    },
-    getData_3 () {
-      let that = this;
-      that.chartData = {};
-      // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
-      const start = '2015Q3';
-      const end = '2016Q3';
-      const base = 'http://127.0.0.1:5000/getVaRsVaRRatioOvertime';
-      axios
-        .get(base, {
-          params: {
-            'start': start,
-            'end': end
-          }
-        })
-        .then(function (response) {
-          let data = response.data
-          let companies = []
-          let series = []
-          for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-              companies.push(key)
-              let chartItem = {}
-              chartItem.name = key
-              chartItem.type = 'line'
-              chartItem.data = []
-              for (const item of data[key]) {
-                chartItem.data.push(item[1])
-              }
-              series.push(chartItem)
-            }
-          }
-          console.log(companies)
-          console.log(series)
-          that.chartData.legend = companies
-          that.lineChartData.xAxisData = ['2015Q3', '2015Q4', '2016Q1', '2016Q2', '2016Q3'] // TODO: write function
-          that.lineChartData.series = series
-          that.drawLineChart3()
+          that.drawLineChart(id)
         });
     }
   }
