cfc4d86ec92b581cf7ec4aa2e29b2108a73fd9b2
Wenzhou Yuan <wenzhouy@uchicago.edu>
2021-03-08 01:07:52 -0600

    Update flaskTovue2.vue

--- demo/frontend/projectlabdemp/src/components/flaskTovue2.vue
+++ demo/frontend/projectlabdemp/src/components/flaskTovue2.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <bar-chart></bar-chart>
+    <stack-bar-chart></stack-bar-chart>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import BarChart from "./BarChart";
+import StackBarChart from "./StackBarChart";
 
 export default {
   name: "my-first-vue",
-  components: {BarChart},
+  components: {BarChart, StackBarChart},
   data: function() {
     return {
       serverResponse: "Waiting for server response"
@@ -19,16 +21,11 @@ export default {
   methods: {
     getData() {
       var that = this;
-      // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
       const path = "http://127.0.0.1:5000/getMsg";
       axios
         .get(path)
         .then(function(response) {
-          // 这里服务器返回的 response 为一个 json object，可通过如下方法需要转成 json 字符串
-          // 可以直接通过 response.data 取key-value
-          // 坑一：这里不能直接使用 this 指针，不然找不到对象
           var msg = response.data.msg;
-          // 坑二：这里直接按类型解析，若再通过 JSON.stringify(msg) 转，会得到带双引号的字串
           that.serverResponse = msg;
 
           alert(
