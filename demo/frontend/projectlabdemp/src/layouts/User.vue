c5cc91fbaa1bbbfde5f3c5c3915829cafe86175a
Wenzhou Yuan <wenzhouy@uchicago.edu>
2021-03-20 02:36:49 -0500

    Add files via upload

---
+++ demo/frontend/projectlabdemp/src/layouts/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <headers />
+  </div>
+</template>
+<script>
+
+import SideMenu from "@/components/SideBar/SideMenu.vue";
+import Headers from "@/components/Headers/Headers.vue";
+
+export default {
+  name: "Admin",
+  components: {
+    SideMenu,
+    Headers
+  }
+}
+</script>
