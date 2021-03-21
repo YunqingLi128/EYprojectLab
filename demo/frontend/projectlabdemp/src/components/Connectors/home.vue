9331ff9f765acde88dd7d5e6db6b23a6d0862c24
yichunlu <yichunlu@usc.edu>
2021-03-21 16:58:11 -0500

    feat: homePage table

--- demo/frontend/projectlabdemp/src/components/Connectors/home.vue
+++ demo/frontend/projectlabdemp/src/components/Connectors/home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div :data="dataInfo">
-    test home page
+  <div>
+    <!-- {{dataInfo}} -->
+    <b-form @submit="onSubmit">
+      <b-form-group id="input-group" label="New Company ID:" label-for="input1">
+        <b-form-input id="input1" v-model="compid" placeholder="Enter the new CompanyID" required></b-form-input>
+      </b-form-group>
+      <b-button block type="submit" variant="primary">Submit</b-button>
+    </b-form>
+    <div>
+    <b-table striped hover :items="items"></b-table>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -11,13 +21,38 @@ export default {
   name: "home",
   data: function () {
     return {
-      dataInfo: {}
+      dataInfo: {},
+      items: [],
+      compid: ''
     }
   },
   mounted () {
-    this.getData();
+    this.items = []
+    this.getData()
   },
   methods: {
+    onSubmit(event){
+      var that = this
+      event.preventDefault()
+      alert(this.compid)
+      const path = "http://127.0.0.1:5000/addDataByID";
+      axios
+        .get(path, {
+          params: {
+            'rssd_id': this.compid
+          },
+          withCredentials: true,
+          headers: {
+            'secret-key': 'super secret key',
+            'Access-Control-Allow-Origin': '*'
+          }
+        })
+        .then(function (response) {
+          let data = response.data;
+          console.log(data);
+          that.getData();
+        });
+    },
     getData () {
       var that = this;
       // root route to init data
@@ -32,6 +67,10 @@ export default {
         })
         .then(function (response) {
           that.dataInfo = response.data;
+          for (let key in that.dataInfo["institutions"]){
+            let temp = {"ID":key, "Name":that.dataInfo["institutions"][key]["Name"], "Nickname":that.dataInfo["institutions"][key]["Nick"]}
+            that.items.push(temp)
+          }
         })
         .catch(function (error) {
           alert("Error " + error);
