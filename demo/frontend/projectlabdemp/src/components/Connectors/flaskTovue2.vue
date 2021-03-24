feea3f586a13739f50874f705c0647868c4b6195
Wenzhou Yuan <wenzhouy@uchicago.edu>
2021-03-24 02:52:16 -0500

    Update flaskTovue2.vue

--- demo/frontend/projectlabdemp/src/components/Connectors/flaskTovue2.vue
+++ demo/frontend/projectlabdemp/src/components/Connectors/flaskTovue2.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <b-form>
+    <b-form inline>
       <b-form-group id="input-group" label="Please input the quarter:" label-for="input-quarter">
         <b-form-input id="input-quarter" name="input-quarter" v-model.trim="quarter"
                       placeholder="example: 2020Q3" required>
         </b-form-input>
       </b-form-group>
-      <b-button @click="getData()" variant="primary" style="margin-bottom: 25px">Search</b-button>
     </b-form>
+    <b-form-group label="Select Company:">
+        <b-form-checkbox-group
+          id="checkbox-group-1"
+          v-model="selected"
+          :options="options"
+          name="Company-Select"
+        ></b-form-checkbox-group>
+    </b-form-group>
+    <b-button @click="getData()" variant="primary">Search</b-button>
     <bar-chart ref="barChart"></bar-chart>
     <stack-bar-chart ref="stackChart"></stack-bar-chart>
     <bar-line-charts ref="barLineChart"></bar-line-charts>
@@ -18,27 +26,49 @@
 import BarChart from '@/components/Cards/BarChart';
 import StackBarChart from '@/components/Cards/StackBarChart';
 import BarLineCharts from '@/components/Cards/BarPlusLineChart';
+import DataSetting from '../../../../../backend/data_setting.json'
 
 export default {
+  created () {
+    let keylist = []
+    let optionCatcher = DataSetting['institutions'];
+    for (let instituions in optionCatcher) {
+      if (optionCatcher.hasOwnProperty(instituions)){
+        keylist.push(instituions)
+      }
+    }
+    let options = []
+    for (let key in keylist) {
+      if (keylist.hasOwnProperty(key)) {
+        let option = {}
+        option.text = optionCatcher[keylist[key]]['Nick']
+        option.value = optionCatcher[keylist[key]]['Nick']
+        options.push(option)
+      }
+    }
+    this.options = options
+  },
   name: 'quarter-charts',
   components: {BarChart, StackBarChart, BarLineCharts},
   data: function () {
     return {
+      selected: [],
+      options: [],
       quarter: ''
     };
   },
   methods: {
     getData () {
-      this.$refs.barChart.getData('VaR-SVaR-comparison', this.quarter)
-      this.$refs.barChart.getData('company-trading-asset-comparison', this.quarter)
-      this.$refs.barChart.getAggData('trading-asset-to-risk-ratio', this.quarter)
-      this.$refs.barChart.getAggData('trading-revenue-to-VaR-ratio', this.quarter)
-      this.$refs.stackChart.getData('standardized-market-risk-weighted-assets-breakdown-by-bank', this.quarter)
-      this.$refs.stackChart.getData('VaR-by-asset-class-and-diversification-effect', this.quarter)
-      this.$refs.barLineChart.getData('trading-asset', this.quarter)
-      this.$refs.barLineChart.getData('trading-liabilities', this.quarter)
-      this.$refs.barLineChart.getData('net-trading-asset', this.quarter)
-      this.$refs.barLineChart.getData('gross-trading-asset', this.quarter)
+      this.$refs.barChart.getData('VaR-SVaR-comparison', this.quarter, this.selected)
+      this.$refs.barChart.getData('company-trading-asset-comparison', this.quarter, this.selected)
+      this.$refs.barChart.getAggData('trading-asset-to-risk-ratio', this.quarter, this.selected)
+      this.$refs.barChart.getAggData('trading-revenue-to-VaR-ratio', this.quarter, this.selected)
+      this.$refs.stackChart.getData('standardized-market-risk-weighted-assets-breakdown-by-bank', this.quarter, this.selected)
+      this.$refs.stackChart.getData('VaR-by-asset-class-and-diversification-effect', this.quarter, this.selected)
+      this.$refs.barLineChart.getData('trading-asset', this.quarter, this.selected)
+      this.$refs.barLineChart.getData('trading-liabilities', this.quarter, this.selected)
+      this.$refs.barLineChart.getData('net-trading-asset', this.quarter, this.selected)
+      this.$refs.barLineChart.getData('gross-trading-asset', this.quarter, this.selected)
     }
   }
 };
