3962b93f148c00cead7723ba0487dbf488ae435f
daocrossover <475970883@qq.com>
2021-03-29 14:36:46 -0700

    change setup.py

--- demo/setup.py
+++ demo/setup.py
@@ -2,11 +2,12 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="backend",
+    name="dashboard",
     version="1.0.0",
     description="Test",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask", "flask_cors", "requests", "urllib3", "pandas", "matplotlib"],
+    setup_requires=["Flask", "flask-cors", "numpy", "pandas"],
+    install_requires=["Flask", "flask-cors", "numpy", "pandas", "requests", "urllib3", "matplotlib"],
 )
