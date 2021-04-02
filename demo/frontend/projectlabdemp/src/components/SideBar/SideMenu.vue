15dffbdec9884149717e9d8dd00a2d1206a1da55
Wenzhou Yuan <wenzhouy@uchicago.edu>
2021-04-02 00:28:10 -0500

    Update SideMenu.vue

--- demo/frontend/projectlabdemp/src/components/SideBar/SideMenu.vue
+++ demo/frontend/projectlabdemp/src/components/SideBar/SideMenu.vue
@@ -21,20 +21,10 @@
       <nav>
         <div class="container-fluid">
           <b-button squared @click="changeStatus()" id="sidebarCollapse" class="navbar-btn" size='sm'>{{toggleStatus}}</b-button>
-            <span></span>
-            <span></span>
-            <span></span>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="nav navbar-nav ml-auto">
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
         </div>
       </nav>
-      <router-view></router-view>
     </div>
+    <router-view id="router-view"></router-view>
   </div>
 </template>
 
@@ -73,6 +63,7 @@ export default {
 <style scoped>
 #sidebarCollapse {
     background: #5A5858;
+    border-color: #5A5858;
     margin-left: -35px;
     margin-top:-36px;
 }
@@ -81,47 +72,28 @@ export default {
     height: 2px;
     margin: 0 auto;
     display: block;
-    background: #555;
+    background: #5A5858;
+    border-color: #5A5858;
     transition: all 0.5s cubic-bezier(0.810, -0.330, 0.345, 1.375);
-}
-#sidebarCollapse span:first-of-type {
-    /* rotate first one */
-    transform: rotate(45deg) translate(2px, 2px);
-}
-#sidebarCollapse span:nth-of-type(2) {
-    /* second one is not visible */
-    opacity: 0;
-}
-#sidebarCollapse span:last-of-type {
-    /* rotate third one */
-    transform: rotate(-45deg) translate(1px, -1px);
-}
-#sidebarCollapse.active span {
-    /* no rotation */
-    transform: none;
-    /* all bars are visible */
-    opacity: 1;
-    margin: 5px auto;
 }
 
 .wrapper {
     display: flex;
     align-items: stretch;
-    perspective: 1500px;
+    perspective: 2000px;
 }
 
 #sidebar {
-    min-width: 250px;
-    max-width: 250px;
+    min-width: 200px;
+    max-width: 200px;
     background: #5A5858;
     color: #fff;
     transition: all 0.6s cubic-bezier(0.945, 0.020, 0.270, 0.665);
-    transform-origin: center left; /* Set the transformed position of sidebar to center left side. */
+    transform-origin: center; /* Set the transformed position of sidebar to center left side. */
 }
 
 #sidebar.active {
-    margin-left: -250px;
-    transform: rotateY(100deg); /* Rotate sidebar vertically by 100 degrees. */
+    margin-left: -200px;
 }
 
 a[data-toggle="collapse"] {
@@ -193,13 +165,21 @@ ul ul a {
     margin-left: auto!important;
 }
 #content {
-  width: 100%;
-  padding: 20px;
-    padding-top: 20px;
-    padding-right: 20px;
-    padding-bottom: 20px;
-    padding-left: 20px;
+  width: 1px;
+  padding: 1px;
+    padding-top: 17px;
+    padding-right: 10px;
+    padding-bottom: 15px;
+    padding-left: 15px;
   min-height: 100vh;
   transition: all 0.3s;
+  background-color: #5A5858;
+}
+
+#router-view {
+  width: 100%;
+  margin: 5px;
+    margin-left: 20px;
+    margin-right: 15px;
 }
 </style>
